b85aa4d552c4c2adbe0e029d247ec2e8963a9ec9
Edilson Lima <edilsonlm217@hotmail.com>
2021-06-01 11:20:39 -0400

    Criação de novas rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,12 @@ import Router from './Router';
 
 import Spinner from '../shared/Spinner';
 
-// const Error404 = lazy(() => import('../error-pages/Error404'));
-// const Error500 = lazy(() => import('../error-pages/Error500'));
-
 const RegisterScreen = lazy(() => import('../app/pages/RegisterScreen'));
 const HomeScreen = lazy(() => import('../app/pages/HomeScreen'));
 const AuthScreen = lazy(() => import('../app/pages/AuthScreen'));
+const SessionScreen = lazy(() => import('../app/pages/SessionScreen'));
+const MessageCustomizationScreen = lazy(() => import('../app/pages/MessageCustomizationScreen'));
+const AttendantScreen = lazy(() => import('../app/pages/AttendantScreen'));
 
 export default function Routes() {
   return (
@@ -24,11 +24,32 @@ export default function Routes() {
           component={HomeScreen}
         />
 
+        <Router
+          exact
+          isPrivate
+          isFullPageLayout
+          path="/session"
+          component={SessionScreen}
+        />
+
+        <Router
+          exact
+          isPrivate
+          isFullPageLayout
+          path="/attendants"
+          component={AttendantScreen}
+        />
+
+        <Router
+          exact
+          isPrivate
+          isFullPageLayout
+          path="/messages"
+          component={MessageCustomizationScreen}
+        />
+
         <Router path="/login" component={AuthScreen} />
         <Router path="/register" component={RegisterScreen} />
-
-        {/* <Router path="/404" component={Error404} />
-        <Router path="/500" component={Error500} /> */}
 
         <Redirect to="/login" />
       </Switch>
