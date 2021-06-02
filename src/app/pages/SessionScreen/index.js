24a34d3ee55b306afe282105e3f3bf812764d0d9
Edilson Lima <edilsonlm217@hotmail.com>
2021-06-02 14:34:04 -0400

    Remoção da lógica de modal

--- src/app/pages/SessionScreen/index.js
+++ src/app/pages/SessionScreen/index.js
@@ -1,50 +1,15 @@
 import React, { useState } from 'react';
-import { Modal } from 'react-responsive-modal';
 
 import './styles.css';
 import 'react-responsive-modal/styles.css';
 
-function SessionScreen() {
-  const [open, setOpen] = useState(false);
-
-  const onOpenModal = () => setOpen(true);
-  const onCloseModal = () => setOpen(false);
-
-  const Step1 = () => {
-    return (
-      <div className="modal-content">
-        <label className="modal-title">Iniciar sessão</label>
-        <label className="modal-subtitle">
-          Este dispositivo será usado como chatbot no momento que você inicializar uma sessão
-        </label>
-
-        <div className="form-group">
-          <label>Whatsapp</label>
-          <input
-            placeholder="Informe seu whatsapp"
-            type="text"
-            className="form-control"
-          />
-        </div>
-
-        <div className="modal-btns-container">
-          <button
-            onClick={onCloseModal}
-            type="button"
-            className="btn btn-dark btn-lg"
-          >
-            Cancelar
-          </button>
-          <button type="button" className="btn btn-primary btn-lg">
-            Próximo
-          </button>
-        </div>
-      </div>
+export default function SessionScreen() {
+  function handleQrCodeScanning() {
+    window.open(
+      'http://localhost:3000/scan',
+      'sharer',
+      'toolbar = 0, status = 0, width = 548, height = 325'
     );
-  }
-
-  function handleAddSession() {
-    onOpenModal();
   }
 
   return (
@@ -53,25 +18,12 @@ function SessionScreen() {
         <h3 className="page-title">Controle de Sessão</h3>
       </div>
 
-      <div className="card" onClick={handleAddSession}>
+      <div className="card" onClick={handleQrCodeScanning}>
         Iniciar sessão
         <span className="menu-icon">
           <i style={{ color: '#00D25B' }} className="mdi mdi-plus-circle"></i>
         </span>
       </div>
-
-      <Modal
-        open={open}
-        onClose={onCloseModal}
-        center
-        closeOnOverlayClick={false}
-        showCloseIcon={false}
-        styles={{ modal: { padding: 0, backgroundColor: '#191C24' } }}
-      >
-        <Step1 />
-      </Modal>
     </div>
   );
 }
-
-export default SessionScreen;
