12cef4370082e33ac718beb01abd3f1a75690436
Edilson Lima <edilsonlm217@hotmail.com>
2021-07-19 10:35:26 -0400

    Inclusão de assets para o app

--- src/app/pages/InvoicesScreen/index.js
+++ src/app/pages/InvoicesScreen/index.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function InvoicesScreen() {
+export default function InvoicesScreen() {
+  const [creditCardHash, setHash] = useState('');
+
+  var cardData = {
+    cardNumber: '5447318579847704',
+    holderName: 'Tereza Santos Farias',
+    securityCode: '675',
+    expirationMonth: '07',
+    expirationYear: '2028'
+  };
+
   return (
-    <h1>InvoicesScreen</h1>
+    <div>
+      <h1>InvoicesScreen</h1>
+
+      <button
+        type="button"
+        className="btn btn-primary btn-fw"
+        onClick={async () => {
+          const PUBLIC_TOKEN = '84EFB588CFAD58116FC279590C81BD0C545F336110FB1023431C4E505AD10D83';
+
+          const checkout = new window.DirectCheckout(PUBLIC_TOKEN, false);
+
+          await checkout.getCardHash(cardData, cardHash => {
+            console.log('------------ DEU CERTO ------------');
+            console.log(cardHash);
+            setHash(cardHash);
+          }, error => {
+            console.log('------------ DEU ERRO ------------');
+            console.log(error);
+          });
+        }}
+      >Assinar</button>
+
+    </div>
   );
 }
-
-export default InvoicesScreen;
