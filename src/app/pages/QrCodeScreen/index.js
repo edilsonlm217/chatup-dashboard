e2004f38b1ddbd920ba14187e3762b039b8f359d
Edilson Lima <edilsonlm217@hotmail.com>
2021-07-19 10:28:20 -0400

    Remoção da lógica de loading

--- src/app/pages/QrCodeScreen/index.js
+++ src/app/pages/QrCodeScreen/index.js
@@ -1,61 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { io } from "socket.io-client";
-import { useHistory } from "react-router-dom";
-import Loader from "react-loader-spinner";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-
-import { useAuth } from '../../../hooks/auth';
+import React from 'react';
+import { useParams } from "react-router-dom";
 
 import './styles.css';
 
 export default function QrCodeScreen() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  const [qrCode, setQrCode] = useState('');
-
-  const { user } = useAuth();
-  const history = useHistory();
-
-  useEffect(() => {
-    const socket = io(`http://localhost:3333/`, {
-      query: {
-        userId: user.id,
-      }
-    });
-
-    socket.on("connect_error", () => {
-      history.push('/error');
-    });
-
-    socket.on('qrcode', data => {
-      const { qrcode } = data;
-      setQrCode(qrcode);
-      setIsLoading(false);
-    });
-
-    socket.on('qrcode-error', () => {
-      history.push('/error');
-    });
-
-    return () => {
-      socket.emit('disconnect');
-      socket.disconnect();
-    };
-
-  }, [history, user.id]);
+  const { qrCode } = useParams();
+  const decoded = JSON.parse(decodeURIComponent(qrCode));
 
   return (
     <div className="container">
-      {isLoading
-        ? <Loader
-          type="Grid"
-          color="#00BFFF"
-          height={100}
-          width={100}
-          timeout={10000} />
-
-        : <img src={qrCode} alt="qrCode" />
-      }
+      <img src={decoded.qrcode} alt="qrCode" />
     </div>
-  )
+  );
 }
